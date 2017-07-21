614e9abc70d60785a94073e9cd811c40d989996b
jerry20071995 <phamthingocthanh95@gmail.com>
2017-07-21 17:36:05 +0700

    list all todo, add new todo

--- todoapp/js/json.js
+++ todoapp/js/json.js
@@ -1,117 +1,164 @@
 $(document).ready(function () {
   var data = [];
-  var todo = '';
 
   getTodo(function(){
-    render();
+    render(function(){
+      renderDoneAction();
+      renderDeleteAction(); 
+    });
   });
-  	
-  	$('.input-group button').attr('disabled', true);
-	  $('.input-group input').keyup(function() {
-	      var empty = true;
-	      $('.input-group input').each(function() {
-	          if ($(this).val().length !== 0) {
-	              empty = false;
-	          }
-	      });
-	      if (empty) {
-	          $('.input-group button').attr('disabled', 'disabled');
-	      } else {
-	          $('.input-group button').attr('disabled', false);
-	      }
-	  });
 
+  $('.input-group button').attr('disabled', true);
+  $('.input-group input').keyup(function() {
+      var empty = true;
+      $('.input-group input').each(function() {
+          if ($(this).val().length !== 0) {
+              empty = false;
+          }
+      });
+      if (empty) {
+          $('.input-group button').attr('disabled', 'disabled');
+      } else {
+          $('.input-group button').attr('disabled', false);
+      }
+  });
 
-	  $('.submit').click(function () {
-	    todo = $('.new-todo').val();
-	    $('.new-todo').val(null);
-	    createTodo(todo);
-	  });
-	  
+  function renderDoneAction() {
+    $( ".done" ).each(function( index ) {
+      $('#'+$(this).attr('id')).click(function(){
+        doneTodo($(this).attr('id').match(/\d+/));
+      });
+      console.log( $(this).attr('id'));
+    });
+  }; 
 
-	    $('.done').click(function() {
-	  		var done = $(this).prent().val();
-	  		alert(done);
-	  });
-	  function render() {
-	    console.log(data);
-	    for (var i in data) {
-	      if (data[i].done == true) {
-	        $('.todo-done').append('<li class="mt5"  attr.index=' + data[i].id + '><button class="btn btn-warning btn-xs done">Delete</button>&nbsp;&nbsp;' + data[i].name + '</li>');
-	      } else {
-	        $('.todo-active').append('<li class="mt5" attr.index=' + data[i].id + '><button class="btn btn-info btn-xs done" >Done</button>&nbsp;&nbsp;' + data[i].name + '</li>');
-	      }
-	    }
-	  }
+  function renderDeleteAction() {
+    $( ".delete" ).each(function( index ) {
+      $('#'+$(this).attr('id')).click(function(){
+        deleteTodo($(this).attr('id').match(/\d+/));
+      });
+      console.log( $(this).attr('id'));
+    });
+  }; 
 
-	  function getTodo(callback) {
-	     $.ajax({
-	      url: 'https://angular-task-list.herokuapp.com/task_lists/100/todos',
-	      type: 'GET',
-	      dataType: 'json',
-	      success: function (responsed_data) {
-	        data = responsed_data;
-	        console.log(JSON.stringify(data));
-	        callback();
-	      },
-	      error: function () {
-	        alert('boo!');
-	      },
-	      beforeSend: setHeader
-	    });
+  $('.submit').click(function () {
+    todo = $('.new-todo').val();
+    $('.new-todo').val(null);
+    createTodo(todo);
+  });
+  
+  function render(callback) {
+    for (var i in data) {
+      if (data[i].done == true) {
+        $('.todo-done').append('<li class="mt5" id="li'+data[i].id+'"><button class="btn btn-warning btn-xs delete" id="delete'+data[i].id+'">Delete</button>&nbsp;&nbsp;'+data[i].name+'</li>');
+      } else {
+        $('.todo-active').append('<li class="mt5" id="li'+data[i].id+'"><button class="btn btn-info btn-xs done" id="done'+data[i].id+'">Done</button>&nbsp;&nbsp;' + data[i].name + '</li>');
+      }
+    }
+    callback();
+  }
 
-	     function setHeader(xhr) {
-	      xhr.setRequestHeader('Content-Type', 'application/json');
-	      xhr.setRequestHeader('Uid', 'abc@gmail.com');
-	      xhr.setRequestHeader('Access-Token', 'mcrMPvs1S31GWFxTBUEBkg');
-	      xhr.setRequestHeader('Client', 'xtBmzMWmXCD2kh2o1HtQNg');
-	    } 
-	  }
+  function getTodo(callback) {
+     $.ajax({
+      url: 'https://angular-task-list.herokuapp.com/task_lists/100/todos',
+      type: 'GET',
+      dataType: 'json',
+      success: function (responsed_data) {
+        data = responsed_data;
+        callback();
+      },
+      error: function () {
+        alert('boo!');
+      },
+      beforeSend: setHeader
+    });
 
+     function setHeader(xhr) {
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Uid', 'abc@gmail.com');
+      xhr.setRequestHeader('Access-Token', 'mcrMPvs1S31GWFxTBUEBkg');
+      xhr.setRequestHeader('Client', 'xtBmzMWmXCD2kh2o1HtQNg');
+    } 
+  }
 
-	  function createTodo(name) {
-	     $.ajax({
-	      type: 'POST',
-	      url: 'https://angular-task-list.herokuapp.com/task_lists/100/todos',
-	      data: JSON.stringify({"name": name}),
-	      dataType: "json",
-	      success: function (responsed_data) {
-	        data.push(responsed_data);
-	      },
-	      error: function (error) {
-	        alert(JSON.stringify(error));
-	      },
-	      beforeSend: setHeader
-	    });
+  function createTodo(name) {
+     $.ajax({
+      type: 'POST',
+      url: 'https://angular-task-list.herokuapp.com/task_lists/100/todos',
+      data: JSON.stringify({"name": name}),
+      dataType: "json",
+      success: function (responsed_data) {
+        data.push(responsed_data);
+        $('.todo-active').append('<li class="mt5" id="li'+responsed_data.id+
+          '"><button class="btn btn-info btn-xs done" id="done'+responsed_data.id+
+          '">Done</button>&nbsp;&nbsp;' + responsed_data.name + '</li>');
+        renderDoneAction();
+      },
+      error: function (error) {
+        alert(JSON.stringify(error));
+      },
+      beforeSend: setHeader
+    });
 
-	    function setHeader(xhr) {
-	      xhr.setRequestHeader('Content-Type', 'application/json');
-	      xhr.setRequestHeader('Uid', 'abc@gmail.com');
-	      xhr.setRequestHeader('Access-Token', 'mcrMPvs1S31GWFxTBUEBkg');
-	      xhr.setRequestHeader('Client', 'xtBmzMWmXCD2kh2o1HtQNg');
-	    }
-	  }
+     function setHeader(xhr) {
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Uid', 'abc@gmail.com');
+      xhr.setRequestHeader('Access-Token', 'mcrMPvs1S31GWFxTBUEBkg');
+      xhr.setRequestHeader('Client', 'xtBmzMWmXCD2kh2o1HtQNg');
+    } 
+  }
 
-		  function doneTodo(done) {
-		     $.ajax({
-		      type: 'GET',
-		      url: 'https://angular-task-list.herokuapp.com/task_lists/100/todos',
-		      data: JSON.stringify({done}),
-		      dataType: "json",
-		      success: function (done) {
-		        alert(done);
-		      },
-		      error: function (error) {
-		        alert(JSON.stringify(error));
-		      },
-		      beforeSend: setHeader
-		    });
+  function doneTodo(todoID) {
+     $.ajax({
+      type: 'PUT',
+      url: 'https://angular-task-list.herokuapp.com/task_lists/100/todos/'+todoID,
+      data: JSON.stringify({"done": true}),
+      dataType: "json",
+      success: function (responsed_data) {
+        console.log('done success'+todoID);
+        let todoName = $('#li'+todoID).text().substring(6);
+        $('#li'+todoID).remove();
+        $('.todo-done').append('<li class="mt5" id="li'+todoID+
+          '"><button class="btn btn-warning btn-xs delete" id="delete'+todoID+
+          '">Delete</button>&nbsp;&nbsp;'+todoName+'</li>');
+        renderDeleteAction();
+      },
+      error: function (error) {
+        alert(JSON.stringify(error));
+      },
+      beforeSend: setHeader
+    });
 
-		    function setHeader(xhr) {
-		      xhr.setRequestHeader('Content-Type', 'application/json');
-		      xhr.setRequestHeader('Uid', 'abc@gmail.com');
-		      xhr.setRequestHeader('Access-Token', 'mcrMPvs1S31GWFxTBUEBkg');
-		      xhr.setRequestHeader('Client', 'xtBmzMWmXCD2kh2o1HtQNg');
-		    }
-		  }
+     function setHeader(xhr) {
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Uid', 'abc@gmail.com');
+      xhr.setRequestHeader('Access-Token', 'mcrMPvs1S31GWFxTBUEBkg');
+      xhr.setRequestHeader('Client', 'xtBmzMWmXCD2kh2o1HtQNg');
+    } 
+  }
+
+  function deleteTodo(todoID) {
+     $.ajax({
+      type: 'DELETE',
+      url: 'https://angular-task-list.herokuapp.com/task_lists/100/todos/'+todoID,
+      dataType: "json",
+      success: function (responsed_data) {
+        console.log('delete success'+todoID)
+        $('#li'+todoID).remove();
+      },
+      error: function (error) {
+        alert(JSON.stringify(error));
+      },
+      beforeSend: setHeader
+    });
+
+     function setHeader(xhr) {
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.setRequestHeader('Uid', 'abc@gmail.com');
+      xhr.setRequestHeader('Access-Token', 'mcrMPvs1S31GWFxTBUEBkg');
+      xhr.setRequestHeader('Client', 'xtBmzMWmXCD2kh2o1HtQNg');
+    } 
+  }
+
 });
+
